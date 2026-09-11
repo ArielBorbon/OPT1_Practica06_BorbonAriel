958dcf7e006bbdd330c10e68cea53f6dcd596ad3
ArielBorbon <ariel.borbon252116@potros.itson.edu.mx>
2026-09-11 03:44:39 -0700

    Practica 6
    Creo que ya quedo

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,36 +1,9 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
-import { AppService } from './app.service.js';
 
 
 
-interface Clase {
-  id: number;
-  nombre: string;
-}
 
 @Controller()
 export class AppController {
-  private clases: Clase[] = [
-    { id: 1, nombre: 'Yoga' },
-    { id: 2, nombre: 'Spinning' },
-    { id: 3, nombre: 'Pilates' },
-  ];
 
-  constructor(private readonly appService: AppService) {}
-
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
-  }
-
-  @Get('clases')
-  obtenerClases(): Clase[] {
-    return this.clases;
-  }
-
-  @Post('clases')
-  agregarClase(@Body() nuevaClase: Clase): Clase {
-    this.clases.push(nuevaClase);
-    return nuevaClase;
-  }
 }
--- src/clases/clases.controller.ts
+++ src/clases/clases.controller.ts
@@ -8,17 +8,14 @@ export class ClasesController {
 
     constructor(private readonly clasesService: ClasesService) { }
 
-    //@Get()
-    //getHello(): string {
-     //   return this.clasesService.getHello();
-  //  }
 
-    @Get('clases')
+
+    @Get()
     obtenerClases(): Clase[] {
         return this.clasesService.listar();
     }
 
-    @Post('clases')
+    @Post()
     agregarClase(@Body() nuevaClase: Clase): Clase {
         return this.clasesService.crear(nuevaClase.nombre);
     }
